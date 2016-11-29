2bc687584feae44257046353a3e953021a1bad56
Gourl.io <easydesignuk@gmail.com>
2016-11-29 20:01:52 +0000

    Update

--- cryptobox.newpayment.php
+++ cryptobox.newpayment.php
@@ -94,17 +94,32 @@ function cryptobox_new_payment($paymentID = 0, $payment_details = array(), $box_
 	.............
 	For example, you have own table `user_orders`...
 	You can use function run_sql() from cryptobox.class.php ( https://gourl.io/api-php.html#run_sql )
+	
 	.............
-	// Save new Bitcoin payment in database
+	// Save new Bitcoin payment in database table `user_orders`
 	$recordExists = run_sql("select paymentID as nme FROM `user_orders` WHERE paymentID = ".intval($paymentID));
 	if (!$recordExists) run_sql("INSERT INTO `user_orders` VALUES(".intval($paymentID).",'".$payment_details["user"]."','".$payment_details["order"]."',".floatval($payment_details["amount"]).",".floatval($payment_details["amountusd"]).",'".$payment_details["coinlabel"]."',".intval($payment_details["confirmed"]).",'".$payment_details["status"]."')");
+	
 	.............
-	// Bitcoin payment confirmed (6+ transaction confirmations)
+	// Received second IPN notification (optional) - Bitcoin payment confirmed (6+ transaction confirmations)
 	if ($recordExists && $box_status == "cryptobox_updated")  run_sql("UPDATE `user_orders` SET txconfirmed = ".intval($payment_details["confirmed"])." WHERE paymentID = ".intval($paymentID));
 	.............
 	.............
+
+	// Onetime action when payment confirmed (6+ transaction confirmations)
+	$processed = run_sql("select processed as nme FROM `crypto_payments` WHERE paymentID = ".intval($paymentID)." LIMIT 1");
+	if (!$processed && $payment_details["confirmed"])
+	{
+		// ... Your code ...
+
+		// ... and update status
+		$sql = "UPDATE crypto_payments SET processed = 1, processedDate = '".gmdate("Y-m-d H:i:s")."' WHERE paymentID = ".intval($paymentID)." LIMIT 1";
+		run_sql($sql);
+	}
+
+	.............
     
-    
+   
 	Debug - new payment email notification for webmaster
 	Uncomment lines below and make any test payment
 	--------------------------------------------
@@ -118,3 +133,4 @@ function cryptobox_new_payment($paymentID = 0, $payment_details = array(), $box_
     return true;
 }
 
+?>
